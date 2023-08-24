c5cc2848841f16c57b5a2b4f66d6479a185c257a
Robert Cheatham <rob@decipher.studio>
2023-08-24 18:55:32 +0200

    deployment changes

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/api/v1/goal-predictions', goalPredictions);
 // CORS
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://labs.thefootballdirective.com");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
